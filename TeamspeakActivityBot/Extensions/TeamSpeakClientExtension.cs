2e5b177e985ece30c13a596e3203617b20bd8ff7
Jan Kesteloo <18596772+cub1con@users.noreply.github.com>
2022-06-15 20:27:43 +0200

    Added TeamSpeakClientExtension

---
+++ TeamspeakActivityBot/Extensions/TeamSpeakClientExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using TeamSpeak3QueryApi.Net.Specialized;
+using TeamSpeak3QueryApi.Net.Specialized.Responses;
+
+namespace TeamspeakActivityBot.Extensions
+{
+    public static class TeamSpeakClientExtension
+    {
+        public static async Task<GetClientInfo[]> GetFullClients(this TeamSpeakClient client)
+        {
+            return (await client.GetClients()).Where(x => x.Type == ClientType.FullClient).ToArray();            
+        }
+    }
+}
