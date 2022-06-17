28d9ddd921100afabef53b90a5d8caf5c9c55728
Cubicon <18596772+cub1con@users.noreply.github.com>
2022-06-17 20:57:39 +0200

    Added TimeSpanExtension, support to get TimeSpan in right format

---
+++ TeamspeakActivityBot/Extensions/TimeSpanExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamspeakActivityBot.Extensions
+{
+    public static class TimeSpanExtension
+    {
+        public static string GetAsDaysAndTime(this TimeSpan timeSpan)
+        {
+            return $"{timeSpan:ddd\\T\\ hh\\:mm\\:ss}";
+        }
+    }
+}
