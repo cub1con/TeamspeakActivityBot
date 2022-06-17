25aaa686d4b385e2770a6eadb9fc93392884b628
Cubicon <18596772+cub1con@users.noreply.github.com>
2022-06-17 20:56:00 +0200

    Added ClientManagerExtension with support for getting client by id

---
+++ TeamspeakActivityBot/Extensions/ClientManagerExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using TeamspeakActivityBot.Manager;
+using TeamspeakActivityBot.Model;
+
+namespace TeamspeakActivityBot.Extensions
+{
+    public static class ClientManagerExtension
+    {
+        public static Client GetUserById(this ClientManager clientManager, int clientId)
+        {
+            return clientManager.Clients.Data.FirstOrDefault(x => x.ClientId == clientId);
+        }
+    }
+}
