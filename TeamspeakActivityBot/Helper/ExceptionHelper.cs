24378ccd093276af0912d71d0d4e12b72d8f1319
Cubicon <18596772+cub1con@users.noreply.github.com>
2022-06-30 21:24:52 +0200

    ExceptionHelper: Switched to a static logger

--- TeamspeakActivityBot/Helper/ExceptionHelper.cs
+++ TeamspeakActivityBot/Helper/ExceptionHelper.cs
@@ -1,18 +1,21 @@
-﻿using Sentry;
+﻿using NLog;
+using Sentry;
 using System;
 
 namespace TeamspeakActivityBot.Helper
 {
     public static class ExceptionHelper
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public static void HandleException(Exception ex, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
         {
-            var logger = NLog.LogManager.GetLogger(memberName);
             int depth = 0;
             do
             {
-                logger.Error(ex, $"Exception #{++depth}: {ex.Message}");
-                logger.Error($"Stacktrace: {ex.StackTrace}");
+                Logger.Error($"{memberName}");
+                Logger.Error(ex, $"Exception #{++depth}: {ex.Message}");
+                Logger.Error($"Stacktrace: {ex.StackTrace}");
                 Console.WriteLine("===========================================");
                 SentrySdk.CaptureException(ex);
             } while ((ex = ex.InnerException) != null);
@@ -21,28 +24,27 @@ namespace TeamspeakActivityBot.Helper
 
         public static void HandleBotException(Exception ex, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
         {
-            var logger = NLog.LogManager.GetLogger(memberName);
             switch (ex)
             {
                 case System.Net.Sockets.SocketException:
                     var socketEx = (System.Net.Sockets.SocketException)ex;
-                    logger.Error(socketEx);
-                    logger.Error($"Socket error: {socketEx.SocketErrorCode.ToString()}");
+                    Logger.Error(socketEx);
+                    Logger.Error($"Socket error: {socketEx.SocketErrorCode}");
                     break;
                 case TeamSpeak3QueryApi.Net.QueryException:
                     var querryEx = (TeamSpeak3QueryApi.Net.QueryException)ex;
-                    logger.Error(querryEx);
+                    Logger.Error(querryEx);
                     if (querryEx.Error != null)
                     {
-                        logger.Error(querryEx.Error.Message);
+                        Logger.Error(querryEx.Error.Message);
                         if (querryEx.Error.Message.ToLower() == ("invalid serverid"))
                         {
-                            logger.Error("Check if instance is running or Setting 'QueryInstanceId' is set. Set to 0 to get default instance from server.");
+                            Logger.Error("Check if instance is running or setting 'QueryInstanceId' is set to the correct instance. Set to 0 to get default instance from server.");
                         }
                     }
                     break;
                 default:
-                    HandleException(ex);
+                    HandleException(ex, memberName);
                     break;
             }
         }
